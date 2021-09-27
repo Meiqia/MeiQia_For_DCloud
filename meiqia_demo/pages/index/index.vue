84939d946ddbfbf6d2d7300f9b8999569411bf9b
zhangshunxing <zhangshunxing@meiqia.com>
2021-09-27 10:31:12 +0800

    [add] 添加美洽uni-app的实例，并且附带ios的原生插件基座

---
+++ meiqia_demo/pages/index/index.vue
@@ -0,0 +1,79 @@
+
+<template>
+	<view class="content">
+		<button type="primary" @click="openMeiQiaChatView()">打开默认的美洽聊天页面</button>
+		<button type="primary" @click="openMeiQiaMessageForm()">打开美洽留言页面</button>
+		<button type="primary" @click="preSendTextMessage()">预发送文字消息</button>
+		<button type="primary" @click="setPreSendProductCardMessage()">发送商品卡片</button>
+		<button type="primary" @click="setClientInfo()">配置个人信息</button>
+	</view>
+</template>
+
+<script>
+    // 首先需要通过 uni.requireNativePlugin("ModuleName") 获取 module 
+    var testModule = uni.requireNativePlugin("MeiQiaChatPlugin");
+    export default {
+        methods: {
+            
+            openMeiQiaChatView() {
+                // 调用同步方法
+                testModule.initChatViewManger();
+				testModule.showMeiQiaChatView();
+            },
+			
+			openMeiQiaMessageForm() {
+			    // 调用同步方法
+			    testModule.showMQMessageForm();
+			},
+			
+			preSendTextMessage() {
+			    // 调用同步方法
+			    testModule.initChatViewManger();
+			    testModule.setPreSendTextMessage("订单号是:48611123");
+				testModule.showMeiQiaChatView();
+			},
+			
+			setPreSendProductCardMessage() {
+				
+				testModule.initChatViewManger();
+				testModule.setPreSendProductCardMessage({
+				pictureUrl : "https://file.pisen.com.cn/QJW3C1000WEB/Product/201701/16305409655404.jpg", 	
+				title: "456456123",
+				description:"描述的内容啊",
+				productUrl:"https://meiqia.com",
+				salesCount:50
+				});
+				
+				testModule.showMeiQiaChatView();
+			},
+			
+			setClientInfo() {
+				
+				testModule.initChatViewManger();
+				testModule.setClientInfo({
+				clientInfo:{
+					age:"22",
+					weixin: "5545666",
+					qq: "45665445"
+					}, 
+				override: true});
+				testModule.showMeiQiaChatView();
+			},
+        }
+    }
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		margin: 100rpx;
+		margin-top: 20rpx;
+	}
+		
+	button{
+		margin: 5rpx;
+	}
+	
+</style>
